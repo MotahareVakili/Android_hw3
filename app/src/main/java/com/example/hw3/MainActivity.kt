ed3026cf720dc793db0fdce0e671d055740c90ef
MotahareVakili <mv2971@yahoo.com>
2024-05-19 01:17:14 +0330

    code_1_problem

--- app/src/main/java/com/example/hw3/MainActivity.kt
+++ app/src/main/java/com/example/hw3/MainActivity.kt
@@ -1,13 +1,4 @@
 package com.example.hw3
-import android.content.Context
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.work.Constraints
-import androidx.work.ExistingPeriodicWorkPolicy
-import androidx.work.NetworkType
-import androidx.work.PeriodicWorkRequestBuilder
-import androidx.work.WorkManager
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -25,8 +16,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.hw3.ui.theme.HW3Theme
-import com.example.hw3.ui.theme.StatusWorker
-import java.util.concurrent.TimeUnit
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,26 +31,12 @@ class MainActivity : ComponentActivity() {
                 HW3_App()
 
             }
-            startPeriodicWorker(this)
+          
         }
     }
 }
 
-fun startPeriodicWorker(context: Context) {
-    val constraints = Constraints.Builder()
-        .setRequiredNetworkType(NetworkType.NOT_REQUIRED)
-        .build()
 
-    val statusCheckRequest = PeriodicWorkRequestBuilder<StatusWorker>(15, TimeUnit.MINUTES)
-        .setConstraints(constraints)
-        .build()
-
-    WorkManager.getInstance(context).enqueueUniquePeriodicWork(
-        "StatusWorker",
-        ExistingPeriodicWorkPolicy.REPLACE,
-        statusCheckRequest
-    )
-}
 
 @Composable
 fun HW3_App() {
