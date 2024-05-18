8f59c467969ed16f62b40f4ea1f37581deb701a0
MotahareVakili <mv2971@yahoo.com>
2024-05-19 01:19:40 +0330

    code_2_problem

--- app/src/main/java/com/example/hw3/MainActivity.kt
+++ app/src/main/java/com/example/hw3/MainActivity.kt
@@ -8,22 +8,6 @@ import androidx.work.ExistingPeriodicWorkPolicy
 import androidx.work.NetworkType
 import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkManager
-import android.content.Intent
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.hw3.ui.theme.HW3Theme
 import com.example.hw3.ui.theme.StatusWorker
 import java.util.concurrent.TimeUnit
@@ -31,16 +15,10 @@ import java.util.concurrent.TimeUnit
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val serviceIntent = Intent(this, InternetService::class.java)
-        startService(serviceIntent)
 
         setContent {
             HW3Theme {
-
                 // A surface container using the 'background' color from the theme
-
-                HW3_App()
-
             }
             startPeriodicWorker(this)
         }
@@ -63,16 +41,4 @@ fun startPeriodicWorker(context: Context) {
     )
 }
 
-@Composable
-fun HW3_App() {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
-    ) {
-        Text(text = "Hello ^_^ ", fontSize = 25.sp)
-        Spacer(modifier = Modifier.height(12.dp))
-        Text(text = Status.InternetStatus, fontSize = 20.sp, color= Color.Magenta)
-    }
-}
 
